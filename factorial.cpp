7d41143604abb3d7b4e185dfbc476b1700c5de19
WAsD-Ddos <mitja3375@gmail.com>
2025-03-24 17:14:22 +0300

    Add project files.

---
+++ factorial.cpp
@@ -0,0 +1,25 @@
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//int factorial(int x) {
+//    int f = 1;
+//    if (x < 0) { return 1; }
+//    if (x >= 0)
+//    {
+//        for (int i = 1; i <= x; i++)
+//            f = f * i;
+//
+//    }
+//    return f;
+//}
+//
+//int main() {
+//	int num;
+//	cout << "input num(fuct)" << endl;
+//	cin >> num;
+//
+//    cout << factorial(num) << endl;
+//
+//	return 0;
+//}
---
+++ fibo.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int fibo(int x) {
+    if (x <= 1) {
+        return x;
+    }
+
+    return fibo(x - 1) + fibo(x - 2);
+
+}
+
+int main() {
+    int num;
+    cout << "input num(fibo)" << endl;
+    cin >> num;
+
+    cout << fibo(num) << endl;
+
+    return 0;
+}
