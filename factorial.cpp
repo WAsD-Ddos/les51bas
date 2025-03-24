cf8d9fcff31d0bd577916169f6cf185b9e8dc158
WAsD-Ddos <mitja3375@gmail.com>
2025-03-24 17:17:23 +0300

    -mistakes

--- factorial.cpp
+++ factorial.cpp
@@ -3,13 +3,11 @@
 //using namespace std;
 //
 //int factorial(int x) {
-//    int f = 1;
 //    if (x < 0) { return 1; }
-//    if (x >= 0)
+//    if (x > 0)
+//    int f = 1;
 //    {
-//        for (int i = 1; i <= x; i++)
-//            f = f * i;
-//
+//        for (int i = 1; i <= x; i++){f = f * i;}
 //    }
 //    return f;
 //}
